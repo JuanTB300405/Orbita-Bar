cfd1a8da61283ea673b3778ba4e7b28194a98fa6
Diego Alejandro Castillo Pérez <DIEGOALEJANDROCASTILLOPEREZ@PASCUALBRAVO.EDU.CO>
2026-03-31 17:57:02 -0500

    Categorias y proveedores listos en diseño y funcionamiento

--- src/pages/proveedores.jsx
+++ src/pages/proveedores.jsx
@@ -20,10 +20,6 @@ const Proveedores = () => {
   const [condicion, setCondicion] = useState(false);
   const [proveedoresData, setProveedoresData] = useState([]);
 
-  const verNoti = () => {
-    setNoti(true);
-  };
-
   const datosFitrados = proveedoresData.filter((provedor) =>
     provedor.nombre.toLowerCase().includes(busqueda.toLowerCase())
   );
@@ -250,29 +246,7 @@ const Proveedores = () => {
     <>
       <section className="proveedores">
         <h1>PROVEEDORES</h1>
-        <div className="proveedores-titulo-linea" >
-          {condicion && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="60"
-              height="60"
-              viewBox="0 0 24 24"
-              fill="#ff1818"
-              className="alertIcon"
-              onClick={verNoti}
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path
-                d="M12 2c5.523 0 10 4.477 10 10a10 
-                10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 
-                -5.393 4.566 -9.72 9.996 -9.72zm.01 13l-.127 .007a1 1 0 0 0 0 
-                1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 
-                -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0
-                1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z"
-              />
-            </svg>
-          )}
-        </div>
+        <div className="proveedores-titulo-linea" ></div>
         <div id="cont">
           <div className="buscador">
             <svg
