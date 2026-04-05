2db5ec962bf0f79384001265d15c85415850524c
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2026-04-05 15:38:30 -0500

    a

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -36,6 +36,27 @@ const Sidebar = () => {
           <li className="li">
             <svg
               xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+              <path d="M10 12h4v4h-4z" />
+            </svg>
+
+            <Link className="link" to="/">
+              Inicio
+            </Link>
+          </li>
+          <li className="li">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
               width="35"
               height="35"
               viewBox="0 0 24 24"
@@ -237,6 +258,27 @@ const Sidebar = () => {
             </svg>
             <Link className="link" to="/categorias">
               Categorias
+            </Link>
+          </li>
+          <li className="li">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M3 10v8l7 -3v-2.6z" />
+              <path d="M3 6l9 3l9 -3l-9 -3z" />
+              <path d="M14 12.3v8.7l7 -3v-8z" />
+            </svg>
+
+            <Link className="link" to="/categorias">
+              Mesas
             </Link>
           </li>
         </ul>
